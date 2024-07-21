95309d69a347517d35ac8993fa46db957700e5a6
Diorgenes Morais <diorgenesmorais@gmail.com>
2024-07-21 20:23:31 -0300

    refactor: remove reducer

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,7 +1,5 @@
-import { createContext, useReducer } from "react";
+import { createContext } from "react";
 import useAuth from "../hooks/useAuth";
-import { AppActions, mainReducer } from "../store/reducers/app.reducer";
-import { IAppState, initialState } from "../store/states/app.state";
 
 export interface IUser {
     isAuthenticated: boolean;
@@ -10,15 +8,12 @@ export interface IUser {
 export interface IAppContext extends IUser {
     login: () => void;
     logout: () => void;
-    state: IAppState;
-    dispatch: React.Dispatch<AppActions>;
 }
 
 export const AppContext = createContext<IAppContext>({} as IAppContext);
 
 export const AppProvider = ({ children }: { children: JSX.Element }) => {
     const { isPending, isAuthenticated, login, logout } = useAuth();
-    const [state, dispatch] = useReducer(mainReducer, initialState);
 
     if (isPending) {
         return <h2>Loading...</h2>;
@@ -26,7 +21,7 @@ export const AppProvider = ({ children }: { children: JSX.Element }) => {
 
     return (
         <AppContext.Provider
-            value={{ isAuthenticated, login, logout, state, dispatch }}
+            value={{ isAuthenticated, login, logout }}
         >
             {children}
         </AppContext.Provider>
